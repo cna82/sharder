eb26775b1733dc83b1b7283ec3933c50282e4bd2
Sina Rahimi <sinarahimi1382@gmail.com>
2025-05-21 11:38:06 +0330

    updating project

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/Components/Header";
 import Footer from "@/Components/Footer/idex";
-import ScrollToTopButton from "@/components/ScrollToTopButton";
+import ScrollToTopButton from "../components/ScrollToTopButton";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
