58446bc94cfad7634a48ac5a27b832a00ffbc798
Sina Rahimi <sinarahimi1382@gmail.com>
2025-05-30 10:28:01 +0330

    commit

--- src/Components/ScrollProgress/index.jsx
+++ src/Components/ScrollProgress/index.jsx
@@ -1,9 +1,13 @@
 "use client";
+import { usePathname } from "next/navigation";
 //imports
 import { useEffect, useState } from "react";
 // ScrollProgress Comp
 const ScrollProgress = () => {
   const [scrollPercentage, setScrollPercentage] = useState(0);
+  const pathname = usePathname();
+  console.log(pathname);
+
   useEffect(() => {
     const handleScroll = () => {
       const scrollTop = window.scrollY || document.documentElement.scrollTop;
@@ -18,15 +22,19 @@ const ScrollProgress = () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+  // conditional rendering for pdf page
+  if (pathname === "/pdf") {
+    return null;
+  }
   return (
     <div
-      className="fixed top-0 left-0 right-0 h-2   bg-gray-200  z-50"
+      className={`fixed top-0 left-0 right-0 h-2   bg-gray-200  z-50`}
       dir="ltr"
     >
       <div
         className="h-full bg-sky-400 transition-all duration-300 ease-out"
         style={{ width: `${scrollPercentage}%` }}
-      ></div>
+      />
     </div>
   );
 };
